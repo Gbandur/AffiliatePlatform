640ecb0812024973a1d9d21aa7afe4a65ae0fa01
Your Name <you@example.com>
2021-02-19 11:55:56 +0400

    fixed-auto-login-controller

--- app/Http/Controllers/Api/Crm/CrmController.php
+++ app/Http/Controllers/Api/Crm/CrmController.php
@@ -8,13 +8,17 @@ use Illuminate\Support\Facades\Http;
 
 class CrmController extends Controller
 {
-    public function autologin(Request $request): int
+    public function autologin(Request $request)
     {
-        $response = Http::post('http://example.com/users', [
+//        $response = Http::post('http://example.com/users', [
+//            'email' => $request->get('email'),
+//            'password' => $request->get('password'),
+//        ]);
+//
+//        return $response->status();
+        return response([
             'email' => $request->get('email'),
-            'password' => $request->get('password'),
-        ]);
-
-        return $response->status();
+            'password' => $request->get('password')
+        ], 200);
     }
 }
